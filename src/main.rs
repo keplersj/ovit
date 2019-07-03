983d23b395a45d37c6b3bd6e8f1006cee1637079
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-02 23:22:18 -0600

    Beginning to reconstruct the partition map into structs

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,142 @@ extern crate positioned_io;
 
 use clap::{App, Arg};
 
+use std::convert::TryInto;
+
 use std::fs::File;
 
 use std::io::prelude::*;
+
+use std::vec::Vec;
 
 use positioned_io::ReadAt;
 
 const TIVO_BOOT_MAGIC: u16 = 0x1492;
 const TIVO_BOOT_AMIGC: u16 = 0x9214;
 const APM_SIGNATURE: u16 = 0x504d;
-const APM_BLOCK_SIZE: u16 = 512;
+const APM_BLOCK_SIZE: usize = 512;
 
-struct Parition {
-    name: &'static str,
-    r#type: &'static str,
+#[derive(Debug)]
+struct Partition {
+    signature: String,
+    partitions_total: u32,
+    starting_sector: u32,
+    sector_size: u32,
+    name: String,
+    r#type: String,
+    starting_data_sector: u32,
+    data_sectors: u32,
+    status: String,
 }
 
+impl Partition {
+    fn new(bytes: Vec<u8>) -> Result<Partition, &'static str> {
+        let signature = String::from_utf8(
+            bytes
+                .get(0..2)
+                .expect("Could not get signature bytes from partition entry")
+                .to_vec(),
+        )
+        .expect("Could not get signature from partition entry")
+        .to_string();
+
+        let partitions_total = u32::from_be_bytes(
+            bytes
+                .get(4..8)
+                .expect("Could not get partitions total from partition entry")
+                .try_into()
+                .unwrap(),
+        );
+
+        let starting_sector = u32::from_be_bytes(
+            bytes
+                .get(8..12)
+                .expect("Could not get starting sector from partition entry")
+                .try_into()
+                .unwrap(),
+        );
+
+        let sector_size = u32::from_be_bytes(
+            bytes
+                .get(12..16)
+                .expect("Could not get sector size from partition entry")
+                .try_into()
+                .unwrap(),
+        );
+
+        let name = String::from_utf8(
+            bytes
+                .get(16..48)
+                .expect("Could not get name bytes from partition entry")
+                .to_vec(),
+        )
+        .expect("Could not get name from partition entry")
+        .trim_matches(char::from(0))
+        .to_string();
+
+        let r#type = String::from_utf8(
+            bytes
+                .get(48..80)
+                .expect("Could not get type bytes from partition entry")
+                .to_vec(),
+        )
+        .expect("Could not get type from partition entry")
+        .trim_matches(char::from(0))
+        .to_string();
+
+        let starting_data_sector = u32::from_be_bytes(
+            bytes
+                .get(80..84)
+                .expect("Could not get the sector where data begins from partition entry")
+                .try_into()
+                .unwrap(),
+        );
+
+        let data_sectors = u32::from_be_bytes(
+            bytes
+                .get(84..88)
+                .expect("Could not get sector size of data from partition entry")
+                .try_into()
+                .unwrap(),
+        );
+
+        let status = format!(
+            "{:#X}",
+            u32::from_be_bytes(
+                bytes
+                    .get(88..92)
+                    .expect("Could not get status from partition entry")
+                    .try_into()
+                    .unwrap(),
+            )
+        );
+
+
+        Ok(Partition {
+            signature,
+            partitions_total,
+            starting_sector,
+            sector_size,
+            name,
+            r#type,
+            starting_data_sector,
+            data_sectors,
+            status
+        })
+    }
+}
+
+#[derive(Debug)]
 struct ApplePartitionMap {
-    partitions: Vec<Parition>,
+    partitions: Vec<Partition>,
 }
 
+#[derive(Debug)]
 struct TivoDrive {
     partition_map: ApplePartitionMap,
 }
 
-fn bytes_to_short(first_byte: u8, second_byte: u8) -> u16 {
-    (u16::from(first_byte) << 8) | u16::from(second_byte)
-}
-
-
-fn open_tivo_image(path: &str) -> Result<(), &'static str> {
-    println!("Reading Tivo Harddrive Disk Image");
+fn open_tivo_image(path: &str) -> Result<TivoDrive, &'static str> {
     let mut file = File::open(path).expect("Couldn't open image");
 
     let mut buffer = [0; 2];
@@ -42,17 +147,17 @@ fn open_tivo_image(path: &str) -> Result<(), &'static str> {
 
     let is_byte_swapped: bool;
 
-    match bytes_to_short(buffer[0], buffer[1]) {
+    match u16::from_be_bytes(buffer[0..2].try_into().unwrap()) {
         TIVO_BOOT_MAGIC => {
-            println!("Disk Image is in Correct Order! (Drive is LittleEndian)");
+            // println!("Disk Image is in Correct Order! (Drive is LittleEndian)");
             is_byte_swapped = false;
         }
         TIVO_BOOT_AMIGC => {
-            println!("Disk Image is Byte Swapped! (Drive is BigEndian)");
+            // println!("Disk Image is Byte Swapped! (Drive is BigEndian)");
             is_byte_swapped = true;
         }
         _ => {
-            println!("I don't think this is a TiVo disk image");
+            // println!("I don't think this is a TiVo disk image");
             return Err("Not a TiVo Drive");
         }
     }
@@ -60,21 +165,43 @@ fn open_tivo_image(path: &str) -> Result<(), &'static str> {
     // The first block on a TiVo drive contain special TiVo magic,
     //  we're not worried about this for reconstructing the partition map.
     //  The partition entry describing the partition map should be in the second block (offet: 512)
-    let mut partition_map_buffer = [0; APM_BLOCK_SIZE as usize];
-    file.read_at(512, &mut partition_map_buffer)
+    let mut partition_map_buffer = [0; APM_BLOCK_SIZE];
+    file.read_exact_at(512, &mut partition_map_buffer)
         .expect("Could not read block containing partition map");
 
-    match bytes_to_short(buffer[0], buffer[1]) {
+    let partition_map_partition: Vec<u8> = partition_map_buffer
+        .chunks_exact(2)
+        .map(|chunk| u16::from_be_bytes(chunk[0..2].try_into().unwrap()))
+        .map(|byte| match is_byte_swapped {
+            true => byte,
+            false => byte.swap_bytes(),
+        })
+        .flat_map(|byte| -> Vec<u8> { byte.to_ne_bytes().to_vec() })
+        .collect();
+
+    match u16::from_be_bytes(partition_map_partition[0..2].try_into().unwrap()) {
         APM_SIGNATURE => {
-            println!("Valid APM Partition entry in the second block!");
-        },
+            // println!("Valid APM Partition entry in the second block!");
+        }
         _ => {
-            println!("Second block does not contain valid APM Partition entry");
-            return Err("Invalid Block at Offset 512")
+            // println!("Second block does not contain valid APM Partition entry");
+            return Err("Invalid Block at Offset 512");
         }
     }
 
-    return Ok(());
+    let driver_descriptor_map = Partition::new(partition_map_partition)
+        .expect("Could not reconstruct Driver Descriptor Map");
+    // println!("Drive Descriptor Map: {:?}", driver_descriptor_map);
+
+    let partition_map = ApplePartitionMap {
+        partitions: vec![driver_descriptor_map],
+    };
+    // println!("Partition Map: {:?}", partition_map);
+
+    let tivo_drive = TivoDrive { partition_map };
+    // println!("Tivo Drive: {:?}", tivo_drive);
+
+    return Ok(tivo_drive);
 }
 
 fn main() {
@@ -90,7 +217,7 @@ fn main() {
     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
     // required we could have used an 'if let' to conditionally get the value)
     let input_path = matches.value_of("INPUT").unwrap();
-    println!("Using input file: {}", input_path);
 
-    open_tivo_image(input_path);
+    let tivo_image = open_tivo_image(input_path).expect("Could not open TiVo Drive Image");
+    println!("{:#?}", tivo_image);
 }
