438db1b2e47a35b1cdc5be746f369853d6a8d284
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-07 13:55:02 -0600

    Impletement unit tests for util functions

--- src/ovit/util.rs
+++ src/ovit/util.rs
@@ -53,3 +53,48 @@ pub fn correct_byte_order(raw_buffer: &[u8], is_byte_swapped: bool) -> Vec<u8> {
         .flat_map(|byte| -> Vec<u8> { byte.to_ne_bytes().to_vec() })
         .collect()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_string_from_bytes_range() {
+        let test_string = b"Hello world!";
+        let string_from_bytes = get_string_from_bytes_range(test_string, 0..=4).unwrap();
+
+        assert_eq!(string_from_bytes, "Hello");
+    }
+
+    #[test]
+    fn test_get_u16_from_bytes_range() {
+        let bytes: [u8; 6] = [0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45];
+        let u16_from_bytes = get_u16_from_bytes_range(&bytes, 0..=1).unwrap();
+
+        assert_eq!(u16_from_bytes, 0xABCD);
+    }
+
+    #[test]
+    fn test_get_u32_from_bytes_range() {
+        let bytes: [u8; 6] = [0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45];
+        let u32_from_bytes = get_u32_from_bytes_range(&bytes, 0..=3).unwrap();
+
+        assert_eq!(u32_from_bytes, 0xABCD_EF01);
+    }
+
+    #[test]
+    fn test_correct_byte_order_if_byte_swap_true() {
+        let bytes: [u8; 2] = [0x92, 0x14];
+        let corrected_bytes = correct_byte_order(&bytes, true);
+
+        assert_eq!(corrected_bytes, [0x14, 0x92]);
+    }
+
+    #[test]
+    fn test_correct_byte_order_if_byte_swap_false() {
+        let bytes: [u8; 2] = [0x14, 0x92];
+        let corrected_bytes = correct_byte_order(&bytes, false);
+
+        assert_eq!(corrected_bytes, [0x14, 0x92]);
+    }
+}
