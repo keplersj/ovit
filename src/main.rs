9743716347fffe5cb179ccfc45d6f6a1aeb301ba
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-07 15:32:41 -0600

    Begin working with schema

--- src/main.rs
+++ src/main.rs
@@ -2,54 +2,92 @@ mod ovit;
 
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
+
+use std::collections::HashMap;
+
+use std::fs;
 
 fn main() {
     let matches = App::new("oViT")
         .version("0.0.0-dev")
         .author("Kepler Sticka-Jones <kepler@stickajones.org>")
         .about("An experimental binary to retrieve MPEG streams from a TiVo hard drive (image) and do other TiVo drive related things.")
-        .arg(Arg::with_name("INPUT")
+        .subcommand(SubCommand::with_name("info").arg(Arg::with_name("INPUT")
             .help("The drive image to read from")
-            .required(true))
+            .required(true)))
+        .subcommand(SubCommand::with_name("schema").arg(Arg::with_name("INPUT")
+            .help("Schema file")
+            .required(true)))
         .get_matches();
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    let input_path = matches.value_of("INPUT").unwrap();
+    match matches.subcommand() {
+        ("info", Some(sub_match)) => {
+            // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+            // required we could have used an 'if let' to conditionally get the value)
+            let input_path = sub_match.value_of("INPUT").unwrap();
 
-    let tivo_image =
-        ovit::TivoDrive::from_disk_image(input_path).expect("Could not open TiVo Drive Image");
+            let tivo_image = ovit::TivoDrive::from_disk_image(input_path)
+                .expect("Could not open TiVo Drive Image");
 
-    let node_count = tivo_image
-        .inodes
-        .iter()
-        .filter(|inode| inode.r#type == ovit::MFSINodeType::Node)
-        .count();
-    let file_count = tivo_image
-        .inodes
-        .iter()
-        .filter(|inode| inode.r#type == ovit::MFSINodeType::File)
-        .count();
-    let stream_count = tivo_image
-        .inodes
-        .iter()
-        .filter(|inode| inode.r#type == ovit::MFSINodeType::Stream)
-        .count();
-    let dir_count = tivo_image
-        .inodes
-        .iter()
-        .filter(|inode| inode.r#type == ovit::MFSINodeType::Dir)
-        .count();
-    let db_count = tivo_image
-        .inodes
-        .iter()
-        .filter(|inode| inode.r#type == ovit::MFSINodeType::Db)
-        .count();
+            let node_count = tivo_image
+                .inodes
+                .iter()
+                .filter(|inode| inode.r#type == ovit::MFSINodeType::Node)
+                .count();
+            let file_count = tivo_image
+                .inodes
+                .iter()
+                .filter(|inode| inode.r#type == ovit::MFSINodeType::File)
+                .count();
+            let stream_count = tivo_image
+                .inodes
+                .iter()
+                .filter(|inode| inode.r#type == ovit::MFSINodeType::Stream)
+                .count();
+            let dir_count = tivo_image
+                .inodes
+                .iter()
+                .filter(|inode| inode.r#type == ovit::MFSINodeType::Dir)
+                .count();
+            let db_count = tivo_image
+                .inodes
+                .iter()
+                .filter(|inode| inode.r#type == ovit::MFSINodeType::Db)
+                .count();
 
-    println!("Node Count: {:#?}", node_count);
-    println!("File Count: {:#?}", file_count);
-    println!("Stream Count: {:#?}", stream_count);
-    println!("Dir Count: {:#?}", dir_count);
-    println!("Db Count: {:#?}", db_count);
+            println!("Node Count: {:#?}", node_count);
+            println!("File Count: {:#?}", file_count);
+            println!("Stream Count: {:#?}", stream_count);
+            println!("Dir Count: {:#?}", dir_count);
+            println!("Db Count: {:#?}", db_count);
+        }
+        ("schema", Some(sub_match)) => {
+            // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+            // required we could have used an 'if let' to conditionally get the value)
+            let input_path = sub_match.value_of("INPUT").unwrap();
+
+            let schema_contents = String::from_utf8(fs::read(input_path).unwrap()).unwrap();
+            let mut schema = HashMap::new();
+
+            for line in schema_contents.split('\n') {
+                let columns: Vec<&str> = line.split(' ').collect();
+                if columns.len() == 8 {
+                    let root = columns[1];
+                    let attr = columns[3];
+
+                    if !schema.contains_key(root) {
+                        schema.insert(root, vec![(attr, columns[4..=7].to_vec())]);
+                    } else {
+                        let mut array = schema.get(root).unwrap().to_vec();
+                        array.push((attr, columns[4..=7].to_vec()));
+                        schema.insert(root, array);
+                    }
+                }
+            }
+
+            println!("{:#?}", schema);
+        }
+        _ => {}
+    }
 }
